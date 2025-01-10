79cbdfe961d6d518d57bf676a2e492f81faabdd2
ZainF24 <165853011+ZainF24@users.noreply.github.com>
2025-01-09 19:05:42 -0600

    fixed int type error

--- gradingCalculator2.cpp
+++ gradingCalculator2.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    int assignments = 0.15;
-    int exams = 0.50;
-    int quizzes = 0.05;
-    int final = 0.25;
-    
+    int assignments = 15;
+    int exams = 50;
+    int quizzes = 5;
+    int final = 25;
+
     cout<<"The weight of each section"<<endl;
     cout<<"Assignment: "<< assignments <<endl;
     return 0;
