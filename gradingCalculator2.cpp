509b2802f1871014aa2b3eb6b59ab54482ccd2f1
ZainF24 <165853011+ZainF24@users.noreply.github.com>
2025-01-09 19:03:19 -0600

    Sections listed and initialized values

--- gradingCalculator2.cpp
+++ gradingCalculator2.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main() {
+    int assignments = 0.15;
+    int exams = 0.50;
+    int quizzes = 0.05;
+    int final = 0.25;
     
+    cout<<"The weight of each section"<<endl;
+    cout<<"Assignment: "<< assignments <<endl;
+    return 0;
 }
