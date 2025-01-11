df6048aae15dda8dc7ef490a4665dc2cd9e8b560
ZainF24 <165853011+ZainF24@users.noreply.github.com>
2025-01-10 22:16:57 -0600

    Outputing text that asks user for graded amount.

--- gradingCalculator2.cpp
+++ gradingCalculator2.cpp
@@ -7,7 +7,21 @@ int main() {
     int quizzes = 5;
     int final = 25;
 
-    cout<<"The weight of each section"<<endl;
-    cout<<"Assignment: "<< assignments <<endl;
+    cout<<"The weight of each section: "<<endl;
+    cout<<"Assignment: "<< assignments <<"%"<<endl;
+    cout<<"Exams: "<< exams <<"%"<<endl;
+    cout<<"Quizzes: "<< quizzes <<"%"<<endl;
+    cout<<"Final Exam: "<< final <<"%"<<endl;
+
+    cout<<"What is the graded percentage for the Assignments section?"<<endl;
+    cout<<"What is the graded percentage for the Exams section?"<<endl;
+    cout<<"What is the graded percentage for the Quizzes section?"<<endl;
+    cout<<"What is the graded percentage for the Final Exam section?"<<endl;
+
+
+
+
+
+
     return 0;
 }
