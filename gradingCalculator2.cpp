c2496138a2184092d344ae615f84da7c59d1e17f
ZainF24 <165853011+ZainF24@users.noreply.github.com>
2025-01-10 22:20:05 -0600

    Initalized graded values and take input

--- gradingCalculator2.cpp
+++ gradingCalculator2.cpp
@@ -6,6 +6,7 @@ int main() {
     int exams = 50;
     int quizzes = 5;
     int final = 25;
+    int gradedAssignments, gradedExams, gradedQuizzes, gradedFinal;
 
     cout<<"The weight of each section: "<<endl;
     cout<<"Assignment: "<< assignments <<"%"<<endl;
@@ -14,9 +15,13 @@ int main() {
     cout<<"Final Exam: "<< final <<"%"<<endl;
 
     cout<<"What is the graded percentage for the Assignments section?"<<endl;
+    cin>>gradedAssignments;
     cout<<"What is the graded percentage for the Exams section?"<<endl;
+    cin>>gradedExams;
     cout<<"What is the graded percentage for the Quizzes section?"<<endl;
+    cin>>gradedQuizzes;
     cout<<"What is the graded percentage for the Final Exam section?"<<endl;
+    cin>>gradedFinal;
 
 
 
